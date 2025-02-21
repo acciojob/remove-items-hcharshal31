4705abbb12abaabdae1f8efe3ad6ab871570a318
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-02-21 17:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let colorSelect = document.getElementById("colorSelect");
+let targetOption = colorSelect.options[colorSelect.selectedIndex]; 
+
+colorSelect.addEventListener("change", (event) => {
+  targetOption = event.target.options[event.target.selectedIndex];
+});
+
+document.getElementById("remove").addEventListener("click", () => {
+  if (targetOption) {
+    targetOption.remove();
+    targetOption = colorSelect.options[0];
+  }
+});
